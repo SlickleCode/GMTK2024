d83e81a90a20ca6f8bee52e64b87505f7728135f
Slickle <slickle.work@gmail.com>
2024-08-17 20:29:13 -0400

    adding rhythm core

--- scripts/conductor.gd
+++ scripts/conductor.gd
@@ -1,7 +1,7 @@
-extends Node2D
+extends AudioStreamPlayer
 
-export var bpm := 100
-export var measures := 4
+@export var bpm := 115
+@export var measures := 4
 
 # Tracking the beat and song position
 var song_position = 0.0
@@ -9,19 +9,17 @@ var song_position_in_beats = 1
 var sec_per_beat = 60.0 / bpm
 var last_reported_beat = 0
 var beats_before_start = 0
-var measure = 1
+var cur_measure_spot = 1
 
 # Determining how close to the beat an event is
 var closest = 0
 var time_off_beat = 0.0
 
 signal beat(position)
-signal measure(position)
-
+signal spot_in_measure(position)
 
 func _ready():
 	sec_per_beat = 60.0 / bpm
-
 
 func _physics_process(_delta):
 	if playing:
@@ -30,37 +28,34 @@ func _physics_process(_delta):
 		song_position_in_beats = int(floor(song_position / sec_per_beat)) + beats_before_start
 		_report_beat()
 
-
 func _report_beat():
 	if last_reported_beat < song_position_in_beats:
-		if measure > measures:
-			measure = 1
+		if cur_measure_spot > measures:
+			cur_measure_spot = 1
 		emit_signal("beat", song_position_in_beats)
-		emit_signal("measure", measure)
+		emit_signal("spot_in_measure", cur_measure_spot)
 		last_reported_beat = song_position_in_beats
-		measure += 1
-
+		cur_measure_spot += 1
 
 func play_with_beat_offset(num):
 	beats_before_start = num
-	$StartTimer.wait_time = sec_per_beat
-	$StartTimer.start()
-
+#	TODO FIX WHY ITS NULL AHHH
+	#$StartTimer.wait_time = sec_per_beat
+	#$StartTimer.start()
 
 func closest_beat(nth):
 	closest = int(round((song_position / sec_per_beat) / nth) * nth) 
 	time_off_beat = abs(closest * sec_per_beat - song_position)
 	return Vector2(closest, time_off_beat)
 
-
 func play_from_beat(beat, offset):
 	play()
 	seek(beat * sec_per_beat)
 	beats_before_start = offset
-	measure = beat % measures
+	cur_measure_spot = beat % measures
 
 
-func _on_StartTimer_timeout():
+func _on_start_timer_timeout():
 	song_position_in_beats += 1
 	if song_position_in_beats < beats_before_start - 1:
 		$StartTimer.start()
