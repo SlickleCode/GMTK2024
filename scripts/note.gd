cd68ba333cf402b6a3fdcf684cd9921d1a9ef160
Slickle <slickle.work@gmail.com>
2024-08-17 20:44:53 -0400

    cleanup and fixing main

---
+++ scripts/note.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@export_range(0, 4) var body_part := 0
+
+var notes = [preload("res://assets/left_foot.png"),
+preload("res://assets/right_foot.png"),
+preload("res://assets/left_hand.png"),
+preload("res://assets/right_hand.png")]
+
+var potential_score = 0
+
+func _ready():
+	body_part = body_part % 4
+	$NotePicture.texture = notes[body_part]
+
+func _on_poor_rating_area_entered(area):
+	potential_score += 1
+
+
+func _on_good_rating_area_entered(area):
+	potential_score += 1
+
+
+func _on_perfect_rating_area_entered(area):
+	potential_score += 1
+
+
+func _on_poor_rating_area_exited(area):
+	potential_score = potential_score - 1
+
+
+func _on_good_rating_area_exited(area):
+	potential_score = potential_score - 1
+
+
+func _on_perfect_rating_area_exited(area):
+	potential_score = potential_score - 1
